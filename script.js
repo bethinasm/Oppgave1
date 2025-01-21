0b7c3ae86c669d30addc4278672b499ba0a25592
Bethina S.M <bethina.s.moen@gmail.com>
2025-01-21 13:17:39 +0100

    :)

--- script.js
+++ script.js
@@ -1,15 +1,72 @@
-function layout1(){
+function noLayout() {
 
 }
 
-function layout2(){
+function verticalLayout() {
     
 }
 
-function layout3(){
+function horizontalLayout() {
     
 }
 
-function layout4(){
+function gridLayout() {
     
+}
+
+
+
+function showTools() {
+    
+    document.getElementById('tools').innerHTML = /*HTML*/`
+    
+    De to viktigste verktøyene vi skal bruke er disse: <br>
+    <ul>
+        <li> 
+                Koderedigeringsprogrammet 
+                <a href="https://code.visualstudio.com/"> 
+                    Visual Studio Code 
+                </a>
+            </li>
+            Vi skal bruke noen <i>extensions</i>:<br>
+            <ul>
+                <li> JavaScript-booster</li>
+                <li> es6-string-html </li>
+                <li> live-server </li>
+                <li> live-share </li>
+            </ul>
+        </li>
+        <li> 
+            Nettleseren 
+            <a href="https://www.google.com/chrome/?brand=FHFK&ds_kid=43700078760035379&gad_source=1&gclid=Cj0KCQiAqL28BhCrARIsACYJvkfNTI-NnigsT5vrwAvtctfwT5lD4yHh4jL-YmvQPMOSEhn7M5x2SK0aAtdKEALw_wcB&gclsrc=aw.ds"> 
+                Google Chrome 
+            </a>
+        </li>
+    </ul>
+    `;
+}
+
+function showHTML() {
+    document.getElementById('html').innerHTML= /*HTML*/ `
+    
+    Vi bruker HTML til å definere et dokument. <br>
+    <ul>
+        <li> Tagger for grunnleggende oppsett av en HTML-fil </li>
+        <li> Tagger for grunnleggende formatering av tekst </li>
+        <li> <tt> < div > </tt> </li>
+        <li> <tt> < input type="text" > </tt> </li>
+        <li> <tt> < button > </tt> </li>
+        <li> <a href="https://www.w3schools.com/html/default.asp">W3Schools HTML Tutorial </a></li>
+        <li> <a href="https://www.w3schools.com/tags/"> W3Schools HTML Reference </a> </li>
+    </ul>
+
+    `;
+}
+
+function showCSS() {
+
+}
+
+function showJS() {
+
 }
